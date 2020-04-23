ffd6678aeab136a844ec811131eda9ec19a087b2
Yash Sanjay Thakkar <yashthakkar2700@gmail.com>
2020-04-23 22:11:53 +0530

    Initialized loadash, normalize.css, webpack, watcher and postcss

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,3 @@
+import "../styles/style.css";
+alert("Hello from updated script");
+alert("Study link!");
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const postCSSPlugins  = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+];
+module.exports = {
+    entry: './app/assets/scripts/app.js',
+    output: {
+        filename: "app.bundled.js",
+        path: path.resolve(__dirname,"app"),
+    },
+    mode: "development",
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader', 
+                    'css-loader?url=false',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: postCSSPlugins
+                        }
+                    }
+                ],
+            }
+        ]
+    }
+}
